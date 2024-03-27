8cba2510d051ac5da7ce113bd66a654ab027f800
William-Gardner-Biotech <will.k.gardner@gmail.com>
2024-03-27 15:32:06 -0500

    Holm bon added

--- bin/manhattan_plot.py
+++ bin/manhattan_plot.py
@@ -4,40 +4,8 @@ import re
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-# Convert the chromosome numbers into one long number set 1-3GbpMMul_10/ncbi_dataset/data/GCF_003339765.1/genomic.gff
-
-# both functions lifted from xtract_genes_from_gff_by_gene_name.py
-
-def build_acc_chrom_dict():
-    with open('accession_no_to_Chrom.csv', 'r') as table:
-        chrom_dict = {}
-        for row in table:
-            r_ow = row.split(',')
-            # [0] is accession number [1] is chrom
-            #print(f"ROW: {r_ow[1]}")
-            chrom_dict[r_ow[0]] = r_ow[1].strip()
-    return(chrom_dict)
-
-# The Mmul_10 ref genome uses NC_ Accession numbers to name the chromosomes instead of numbering or chr1 etc. This command will replace those acc numbers with their respective chrom number for genes
-def replace_acc_with_chrom(chrom_dict: dict):
-    if os.path.exists(os.path.join(os.getcwd(), "Mmul10_chrom_converted.gff")):
-        return print(f'Running redundant function, Converted GFF already exists {os.path.join(os.getcwd(), "Mmul10_chrom_converted.gff")}')
-    else:
-        with open("MMul_10/ncbi_dataset/data/GCF_003339765.1/genomic.gff", 'r') as filtered_gff, open("Mmul10_chrom_converted.gff", 'w') as out_bedfile:
-            pattern = r"Name=([^;]+)"
-            for entry in filtered_gff:
-                # skip headers
-                if entry.startswith('#'):
-                    continue
-                if "ID=gene" in entry:
-                    ent_ry = entry.split('\t')
-                    #[0]=accession,[3]=start,[4]=end,[5]=score,[6]=strand,[8]=description
-                    #print(ent_ry)
-                    try:
-                        bed_line = f"{chrom_dict[ent_ry[0]]}\t{ent_ry[3]}\t{ent_ry[4]}\t{re.search(pattern, ent_ry[8]).group(1)}\t{ent_ry[5]}\t{ent_ry[6]}\n"
-                    except: pass
-                    out_bedfile.write(bed_line)
+from accession_to_chr import build_acc_chrom_dict
+from statsmodels.stats.multitest import multipletests
 
 def chr_to_genomic_coords(genomic_gff):
     # Now we need to make a conversion that will build a list of all chromosome positions and add the 
@@ -120,9 +88,29 @@ def gather_dots(genomic_chr_dict, association_file):
     # Add -log10 scale to make the smaller p-values stand out graphically
     df['neg_log10_p'] = df['p-value'].apply(lambda x: -np.log10(float(x)))
 
-    return df
+    df['p-value'] = df['p-value'].astype(float)
 
-def generate_plot(df):
+    # Adjust the p-values using Hol-Bonferroni
+    def adjust_p_values(p_values, chosen_method='holm'):
+        # function will return a tuple 0 = reject, 1 = adjusted p-value, 2 = Sidak adjusted, 3 = Bonferroni adjusted
+        return multipletests(p_values, method = chosen_method, is_sorted = False, alpha = 0.05)
+    
+    df['reject'] = adjust_p_values(df['p-value'])[0]
+    df['adj_p'] = adjust_p_values(df['p-value'])[1]
+
+    #pd.set_option('display.max_rows', None)
+    #print(df.head(500))
+
+    #print('Adjusted')
+    #print(df['adj_p'].unique())
+
+    adj_df = (df[df['reject'] == True])
+    adj_df['neg_log10_p'] = df['adj_p'].apply(lambda x: -np.log10(float(x)))
+
+    return (df, adj_df)
+
+
+def generate_plot(df, title = 'Manhattan plot (All p-values)'):
     '''
     Function that will generate the manhattan plot and add coloring based upon chromosome
     '''
@@ -140,14 +128,15 @@ def generate_plot(df):
         chr_names.append(chr_name)
         chr_positions.append(chr_df['Genomic_Position'].mean()) # finding avg of the group to add the middle x-tick
 
-    # Bonferroni is way too high, let's exclude all p-values == 1
-    bad_observations = ((df['p-value']) == '1.0').sum()
-    print(bad_observations)
+    # Any obvious p-value ~> 0.75 should be discarded for bonferroni
+    # df['float_p'] = df['p-value'].astype(float)
+    obvious_obs = (df['p-value'] >= 0.5).sum()
+    print(f"OBV OBS: {obvious_obs}")
 
     # Calculate the Bonferroni p-value to create threshold for p-value over false positives
-    Bonferroni = 0.05 / (df.shape[0] - bad_observations)
+    Bonferroni = 0.05 / (df.shape[0])
 
-    print(f'Bonferroni corrected p-value: {Bonferroni}')
+    print(f'Bonferroni corrected p-value: {Bonferroni}\nBonferroni minus Obvious OBV: {0.05 / (df.shape[0]- obvious_obs)}')
     # Add a horizontal dotted line at the threshold -log10 value
     plt.axhline(y=(-np.log10(Bonferroni)), color='gray', linestyle=':', linewidth=1)
     plt.axhline(y=(-np.log10(0.05)), color='red', linestyle=':', linewidth=1)
@@ -157,8 +146,27 @@ def generate_plot(df):
     plt.ylabel('-log10 p-values')
     # Set x-axis ticks and labels
     #plt.xticks(chr_positions, chr_names)
+    handles, labels = plt.gca().get_legend_handles_labels()
+    plt.legend(handles, labels, title='CHROMOSOME', bbox_to_anchor=(1.05, 1), loc='upper left')
+
+    plt.title(title)
+
     plt.show()
 
+    plt.clf()
+
+    for chr_name, chr_df in chr_groups:
+        plt.scatter(chr_df['Genomic_Position'], chr_df['neg_log10_p'], label=chr_name)
+
+        chr_names.append(chr_name)
+        chr_positions.append(chr_df['Genomic_Position'].mean())
+    
+    # TODO fixed 26Mar24 MAF = 0.05 to remove the streaks caused by low maf inclusion, left in as a way to visualize the SNP counts
+    #pd.set_option('display.max_rows', None)
+    #print(f'Value Counts:\n {df["p-value"].value_counts()}')
+    #print('LOG 10 P == 9.338377:')
+    #df['neg_log10_p'] = df['neg_log10_p'].astype(float)
+    #print(df[(df['neg_log10_p']>=9.3) & (df['neg_log10_p'] <= 9.4)])
 
 
 
@@ -181,8 +189,12 @@ def main():
     # Build the relational dict so we can begin converting points for the manhattan plot
     genome_chr_coords = chr_to_genomic_coords(args.gff)
 
-    plot_df = gather_dots(genome_chr_coords, args.assoc)
+    plot_df, adj_df = gather_dots(genome_chr_coords, args.assoc)
 
+    # Default df plot
     generate_plot(plot_df)
+
+    # Holm-Bonferroni Adj manhattan
+    generate_plot(adj_df, 'Manhattan plot (Holm-Bon significant SNPs)')
 
 main()
